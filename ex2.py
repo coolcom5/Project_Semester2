ff951937343d3b4c8c609378441e65c24c135754
michael mcmahon <coolcom5@gmail.com>
2025-03-17 14:44:38 +0800

    Add Ex2.py

---
+++ ex2.py
@@ -0,0 +1,15 @@
+# Ex2.py
+# Enter a name and convert to uppercase
+# Author: Michael McMahon
+# DATE: 17/2/25
+
+# enter a name
+x=input("Enter a Name: ")
+
+# double the number
+res=x.upper()
+
+# dispaly the result
+print("The Name in uppercase is: ")
+print(res)
+
