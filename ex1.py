d7d6501b788ae88e4fc4ba63c8120727fb39a90e
michael mcmahon <coolcom5@gmail.com>
2025-03-17 14:16:13 +0800

    Add ex1.py to repository

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# enter a number and double it
+# Author: Michael
+# DATE: 17/2/25
+# Program to enter a number and then output it doubled
+
+# enter a number
+x=input("Enter a Number: ")
+
+# double the number
+res=int(x) * 2
+
+# dispaly the result
+print("The result is: ")
+print(res)
+
